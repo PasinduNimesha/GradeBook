a505b1b2f66afe435b954b793a7ded9de5847fb2
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-12 10:22:14 +0530

    Create BottomNavBar.dart

---
+++ lib/components/BottomNavBar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const BottomNavBar({
+    required this.currentIndex,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedIconTheme: const IconThemeData(color: Colors.red),
+      unselectedIconTheme: const IconThemeData(color: Colors.red),
+      selectedLabelStyle: const TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+      unselectedLabelStyle: const TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          activeIcon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.location_on_outlined),
+          activeIcon: Icon(Icons.location_on),
+          label: 'Location',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          activeIcon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
