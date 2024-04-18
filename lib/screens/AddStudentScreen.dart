5e309f48bfcacd9b899f9ac966de43127e5af53e
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-18 18:16:41 +0530

    Create AddStudentScreen.dart

---
+++ lib/screens/AddStudentScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AddStudentScreen extends StatelessWidget {
+  const AddStudentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
