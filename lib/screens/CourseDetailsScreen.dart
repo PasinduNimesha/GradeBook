806df16aaf2678cab32ae562e6f15786dfae076e
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-19 20:14:37 +0530

    Update CourseDetailsScreen.dart

--- lib/screens/CourseDetailsScreen.dart
+++ lib/screens/CourseDetailsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CourseDetailsscreen extends StatefulWidget {
   const CourseDetailsscreen({super.key});
@@ -10,6 +11,17 @@ class CourseDetailsscreen extends StatefulWidget {
 class _CourseDetailsscreenState extends State<CourseDetailsscreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Course Details Screen'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('Course Details Screen'),
+          ],
+        ),
+      ),
+    );
   }
 }
