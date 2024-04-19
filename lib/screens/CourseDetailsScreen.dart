e645bb872e4a9da4dd291aa766a24e06394a7db2
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-19 19:41:59 +0530

    Create CourseDetailsScreen.dart

---
+++ lib/screens/CourseDetailsScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CourseDetailsscreen extends StatefulWidget {
+  const CourseDetailsscreen({super.key});
+
+  @override
+  State<CourseDetailsscreen> createState() => _CourseDetailsscreenState();
+}
+
+class _CourseDetailsscreenState extends State<CourseDetailsscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
