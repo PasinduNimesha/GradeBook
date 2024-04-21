498c0849f022f8ad138061015b22f255c538b869
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-21 20:19:21 +0530

    Update InputField.dart

--- lib/components/InputField.dart
+++ lib/components/InputField.dart
@@ -3,19 +3,30 @@ import 'package:flutter/material.dart';
 class InputField extends StatefulWidget {
   final String label;
   final bool obscureText;
+  final String? defaultValue; // New property for default value
   final ValueChanged<String>? onPressed;
-  const InputField({super.key, required this.label, required this.onPressed, this.obscureText = false});
+
+  const InputField({super.key, required this.label, required this.onPressed, this.obscureText = false, this.defaultValue});
 
   @override
   State<InputField> createState() => _InputFieldState();
 }
 
 class _InputFieldState extends State<InputField> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.defaultValue ?? ''); // Set default value to the controller
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 18.0),
       child: TextField(
+        controller: _controller, // Assign controller to the TextField
         decoration: InputDecoration(
           labelText: widget.label,
           labelStyle: const TextStyle(
@@ -27,5 +38,11 @@ class _InputFieldState extends State<InputField> {
         obscureText: widget.obscureText,
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the controller when the widget is disposed
+    super.dispose();
   }
 }
