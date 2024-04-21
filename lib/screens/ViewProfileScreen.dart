9e2006c4ec00fada8b004e6910a16b332ef7e76d
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-22 00:12:44 +0530

    Update ViewProfileScreen.dart

--- lib/screens/ViewProfileScreen.dart
+++ lib/screens/ViewProfileScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ViewProfileScreen extends StatelessWidget {
-  const ViewProfileScreen({super.key});
+  final String documentID;
+  const ViewProfileScreen({super.key, required this.documentID});
 
   @override
   Widget build(BuildContext context) {
@@ -9,19 +11,43 @@ class ViewProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('View Profile Screen'),
       ),
-      body: const Center(
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnwxypGUcF0-hZwuAgd4cucn2o8Zk6t1d9Jw5mik5S_Dpd3A9bw1Tl9eS-4G0fFmQojUQ&usqp=CAU'),
-            ),
-            SizedBox(height: 20),
-            Text('John Doe'),
-            SizedBox(height: 20),
-            Text('')
-          ],
-        ),
+      body: FutureBuilder<DocumentSnapshot>(
+        future: FirebaseFirestore.instance.collection('students').doc(documentID).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('No data found'));
+          } else {
+            var userData = snapshot.data!.data() as Map<String, dynamic>?;
+            return Center(
+              child: Column(
+                children: [
+                  const CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnwxypGUcF0-hZwuAgd4cucn2o8Zk6t1d9Jw5mik5S_Dpd3A9bw1Tl9eS-4G0fFmQojUQ&usqp=CAU',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text('First Name: ${userData!['first_name']}'),
+                  const SizedBox(height: 10),
+                  Text('Last Name: ${userData['last_name']}'),
+                  const SizedBox(height: 10),
+                  Text('Email: ${userData['email']}'),
+                  const SizedBox(height: 10),
+                  Text('Phone: ${userData['phone']}'),
+                  const SizedBox(height: 10),
+                  Text('Address: ${userData['address']}'),
+                  const SizedBox(height: 10),
+                  Text('Age: ${userData['age']}'),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
