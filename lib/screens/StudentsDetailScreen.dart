8b497bb07a79561e77fc9ac4e6c89b68a883b4e3
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-22 08:42:34 +0530

    Update StudentsDetailScreen.dart

--- lib/screens/StudentsDetailScreen.dart
+++ lib/screens/StudentsDetailScreen.dart
@@ -39,26 +39,33 @@ class _StudentsDetailScreenState extends State<StudentsDetailScreen> {
           if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
             return const Center(child: Text('No students found'));
           }
-          return SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
+          return Center(
             child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: DataTable(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    top: BorderSide(width: 1),
-                    bottom: BorderSide(width: 1),
-                  ),
+              scrollDirection: Axis.horizontal,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    DataTable(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(width: 1),
+                          bottom: BorderSide(width: 1),
+                        ),
+                      ),
+                      border: TableBorder.all(),
+                      columns: const [
+                        DataColumn(label: Text('Profile')),
+                        DataColumn(label: Text('First Name'),),
+                        DataColumn(label: Text('Last Name')),
+                        DataColumn(label: Text('Age')),
+                        DataColumn(label: Text('Edit')),
+                      ],
+                      rows: _buildRows(snapshot.data!.docs),
+                    ),
+                  ],
                 ),
-                border: TableBorder.all(),
-                columns: const [
-                  DataColumn(label: Text('Profile')),
-                  DataColumn(label: Text('First Name'),),
-                  DataColumn(label: Text('Last Name')),
-                  DataColumn(label: Text('Age')),
-                  DataColumn(label: Text('Edit')),
-                ],
-                rows: _buildRows(snapshot.data!.docs),
               ),
             ),
           );
