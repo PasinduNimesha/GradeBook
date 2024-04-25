925dd1dcf4f6cf1339281426a4bf92bbd1c376e8
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-26 00:01:19 +0530

    Update AddCourseScreen.dart

--- lib/screens/AddCourseScreen.dart
+++ lib/screens/AddCourseScreen.dart
@@ -18,28 +18,30 @@ class _AddCourseScreenState extends State<AddCourseScreen> {
       appBar: AppBar(
         title: const Text('Add Course Screen'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            InputField(label: "Enter Course Name", onPressed: (value) {
-              setState(() {
-                _courseName = value;
-              });
-            },
-            ),
-            InputField(label: "Enter Course Code", onPressed: (value) {
-              setState(() {
-                _courseCode = value;
-              });
-            },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(onPressed: () {
-              addCourse();
-              }, child: const Text('Add Course')
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              InputField(label: "Enter Course Name", onPressed: (value) {
+                setState(() {
+                  _courseName = value;
+                });
+              },
+              ),
+              InputField(label: "Enter Course Code", onPressed: (value) {
+                setState(() {
+                  _courseCode = value;
+                });
+              },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(onPressed: () {
+                addCourse();
+                }, child: const Text('Add Course')
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -50,7 +52,17 @@ class _AddCourseScreenState extends State<AddCourseScreen> {
       'name': _courseName,
       'code': _courseCode,
     }).then((value) {
-      Navigator.pop(context);
+      showDialog(context: context, builder: (context) {
+        return AlertDialog(
+          title: const Text('Course Added'),
+          content: const Text('Course has been added successfully'),
+          actions: [
+            TextButton(onPressed: () {
+              Navigator.pop(context);
+            }, child: const Text('OK'))
+          ],
+        );
+      });
     });
   }
 }
