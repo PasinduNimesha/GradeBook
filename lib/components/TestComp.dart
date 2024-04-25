01b7db6937ca30371003f31733c06fc7656ee8ab
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-26 00:06:41 +0530

    Create TestComp.dart

---
+++ lib/components/TestComp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test'),
+      ),
+      body: const Center(
+        child: Text('Test'),
+      ),
+    );
+  }
+}
