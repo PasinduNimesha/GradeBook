0f5bb219095eabe2e15d90cdf453da9836a6a9ce
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-25 23:45:07 +0530

    Update AddCourseScreen.dart

---
+++ lib/screens/AddCourseScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AddCourseScreen extends StatefulWidget {
+  const AddCourseScreen({super.key});
+
+  @override
+  State<AddCourseScreen> createState() => _AddCourseScreenState();
+}
+
+class _AddCourseScreenState extends State<AddCourseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Course Screen'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/add-student');
+              },
+              child: const Text('Add Student'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/add-marks');
+              },
+              child: const Text('Add Marks'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
