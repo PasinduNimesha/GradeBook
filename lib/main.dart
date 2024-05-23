22f47c54588f3fafb04432b35a2e49a5f89ed72f
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-23 16:52:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:grade_book/screens/LoginScreen.dart';
+import 'package:grade_book/screens/MainScreen.dart';
 
 import 'firebase_options.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const MainScreen(),
     );
   }
 }
