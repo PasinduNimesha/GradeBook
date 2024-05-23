db93ac9373589464d4f087c0e36c0e3b2d5ef837
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-23 18:14:35 +0530

    Update SettingsScreen.dart

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../components/SettingsCard.dart';
+import 'LoginScreen.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -40,9 +43,41 @@ class SettingsScreen extends StatelessWidget {
               icon: Icons.info,
               onTap: () {},
             ),
+            SizedBox(height: 40),
+            ElevatedButton(onPressed: () {
+              showDialog(context: context, builder: (context) {
+                return AlertDialog(
+                  title: const Text('Log Out'),
+                  content: const Text('Are you sure you want to log out?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        SharedPreferences prefs = await SharedPreferences.getInstance();
+                        prefs.remove('username');
+                        Get.offAll(() => const LoginScreen());
+                      },
+                      child: const Text('Log Out'),
+                    ),
+                  ],
+                );
+              });
+            }, child: Text('Log Out'))
           ],
         ),
       ),
     );
   }
+  void logOut() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("isLoggedIn", false);
+    Get.to(const LoginScreen());
+  }
 }
+
+
