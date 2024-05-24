87a3368e3452a8068b02c1b3e6be57e0272047a0
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-24 17:20:30 +0530

    Update UpperHome.dart

--- lib/components/UpperHome.dart
+++ lib/components/UpperHome.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class UpperHome extends StatelessWidget {
+class UpperHome extends StatefulWidget {
   const UpperHome({super.key});
 
   @override
+  State<UpperHome> createState() => _UpperHomeState();
+}
+
+class _UpperHomeState extends State<UpperHome> {
+  int _numberOfCourses = 0;
+  int _numberOfStudents = 0;
+  @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.3,
+      height: MediaQuery.of(context).size.height * 0.2,
       width: MediaQuery.of(context).size.width * 0.9,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -20,6 +27,59 @@ class UpperHome extends StatelessWidget {
           ),
         ],
       ),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.book,
+                  size: 50,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Courses',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.person,
+                  size: 50,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Profile',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
